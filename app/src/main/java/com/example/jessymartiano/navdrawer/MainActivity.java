02ba954e57a5287cd59a7cc4848901e7cef900dd
jessymartiano <martiano.jessy@gmail.com>
2017-01-25 12:11:25 +0200

    jessyyyyyy a reussi quelque chose!!

--- app/src/main/java/com/example/jessymartiano/navdrawer/MainActivity.java
+++ app/src/main/java/com/example/jessymartiano/navdrawer/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-    //push
+    //push 2 bis
     @Override
     protected void onStart() {
         super.onStart();
